66b6165d054c32c3e57d730881b6b86f0a185cce
Jose David Arevalo <jarevalo@omnibnk.com>
2019-08-12 17:18:16 -0500

    fixed key value

--- producer_consumer_transactions.py
+++ producer_consumer_transactions.py
@@ -25,7 +25,7 @@ def publish_message(producer_instance, topic_name, key, value):
     try:
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
-        producer_instance.send(topic_name, key=key, value=value_bytes)
+        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
         producer_instance.flush()
         print('Message published successfully.')
     except Exception as ex:
